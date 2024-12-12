874f39dfe33bbed6f1f83fde47e929065162937a
bhavana madas <80811054+bhavana791@users.noreply.github.com>
2024-12-12 13:27:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Task from './screens/Task';
+import { ToastContainer } from 'react-toastify';
+
+function App() {
+  return (
+    <div>
+      
+      <Task/>
+      <ToastContainer />
+
+    </div>
+  );
+}
+
+export default App;
+
+
